d4ae6878a98d1ef2880d672c47c15eb1c4abe7ab
Thiago <thberenger27@gmail.com>
2023-12-19 13:25:47 -0300

    att taurus
    midia page, main css fixed, + js

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+//---- event mouse Over Taurus Season ----
+
+
+const taurusBox = document.querySelector('.main-video')
+
+
+taurusBox.addEventListener('click', function() {
+    window.location.href = './midia-page/midia-pubs.html'
+})
