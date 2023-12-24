917366554bc302fc95b168ebe44a5e9957b1b88b
lucasberenger <thegreatberenger@gmail.com>
2023-12-23 23:18:14 -0300

    added responsiveness to main page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,3 +14,12 @@ flashBack.addEventListener('click', function(){
     window.location.href = './midia-page/flashback.html'
 })
 
+// ------- Menu Mobile ------------ //
+const menuMobile = document.getElementById('menu-mobile');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+menuMobile.addEventListener('click', toggleMenu);
